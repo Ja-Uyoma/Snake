b89eb89c8aeb74dee0827a26b6791efac8eb5fbc
Jimmy Givans <jauyoma.dev@gmail.com>
2024-11-17 12:08:49 +0300

    feat: create a window with a black background

--- source/main.cpp
+++ source/main.cpp
@@ -1,6 +1,25 @@
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Window/Event.hpp>
+#include <SFML/Window/VideoMode.hpp>
+#include <SFML/Window/Window.hpp>
 #include <cstdlib>
 
 int main()
 {
+  sf::RenderWindow window(sf::VideoMode(640, 480), "Snake");
+
+  while (window.isOpen()) {
+    sf::Event event;
+
+    while (window.pollEvent(event)) {
+      if (event.type == sf::Event::Closed) {
+        window.close();
+      }
+    }
+
+    window.clear();
+    window.display();
+  }
+
   return EXIT_SUCCESS;
 }
