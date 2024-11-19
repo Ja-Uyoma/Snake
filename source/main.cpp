42c2272e3c88771287af6c4fca156b5e0a4920c2
Jimmy Givans <jauyoma.dev@gmail.com>
2024-11-19 12:26:21 +0300

    refactor: use the Game class to start and run the game in main()

--- source/main.cpp
+++ source/main.cpp
@@ -1,38 +1,10 @@
-#include "Constants.hpp"
-#include "Fruit.hpp"
-#include <SFML/Graphics/RenderWindow.hpp>
-#include <SFML/Window/Event.hpp>
-#include <SFML/Window/VideoMode.hpp>
-#include <SFML/Window/Window.hpp>
+#include "Game.hpp"
 #include <cstdlib>
-
-namespace snake {
-
-void Main()
-{
-  sf::RenderWindow window(sf::VideoMode(Window::Width, Window::Height), "Snake");
-  Fruit const fruit {sf::Vector2i(Window::Width / 2, Window::Height / 2)};
-
-  while (window.isOpen()) {
-    sf::Event event;
-
-    while (window.pollEvent(event)) {
-      if (event.type == sf::Event::Closed) {
-        window.close();
-      }
-    }
-
-    window.clear();
-    window.draw(fruit.getShape());
-    window.display();
-  }
-}
-
-}   // namespace snake
 
 int main()
 {
-  snake::Main();
+  snake::Game const game {};
+  game.run();
 
   return EXIT_SUCCESS;
 }
